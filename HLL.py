9800ae73e19d8f01450fbf6a941f363b349852ed
person <a>
2019-11-03 11:22:22 -0500

    Add HLL.py

---
+++ HLL.py
@@ -0,0 +1,20 @@
+# HyperLogLog implementation for our project
+# Using cardinality estimation algorithms from:
+    # Ertl, O. (2017). New cardinality estimation algorithms for HyperLogLog sketches. ArXiv.
+
+class HLL:
+    """HyperLogLog implementation"""
+
+    def __init__(self, p):
+        """Initialize HLL with 2^p registers"""
+        self.p = p
+        self.q = 64 - p # assuming 64-bit hash value
+        self.m = 2 ** p
+
+    def insert(self, read):
+        """Insert read into the HLL"""
+        return
+
+    def cardinality(self):
+        """Estimate cardinality of the HLL"""
+        return
