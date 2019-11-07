ed7e3ef901c5ed2b502bd6a1dc36678c3264ce36
justg99 <justg99@gmail.com>
2019-11-07 14:48:12 -0500

    Added base of Cardinalities

--- Cardinality.py
+++ Cardinality.py
@@ -1,5 +1,19 @@
-def getMultiplicity(hll):
-    return
 
-def estimateCardinality(counts):
-    return
+'''Algorithm 4'''
+def getMultiplicity(K):
+    m = len(K)
+    C = {}
+    # Fills C with times K[i] appears in K
+    # K[i] is a value in the range [1, q+1]
+    for i in range(1, m):
+        if K[i] not in C:
+            C[K[i]] = 1
+        else:
+            C[K[i]] += 1
+    return C
+
+
+'''Algorithm 8'''
+def estimateCardinality(C):
+    m = len(C)
+    if C[q+1] = m
