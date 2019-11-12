6e02d6dcd784e2fef587cad6bbe525a73f0218b8
gautam-prab <gautam-prab@users.noreply.github.com>
2019-11-11 20:25:19 -0500

    Fix plot similarity

--- PlotSimilarityAccuracy.py
+++ PlotSimilarityAccuracy.py
@@ -10,12 +10,13 @@ def main():
     h2 = HLL(12)
 
     num_jacs = 100
-    jaccards = np.linspace(0,0.5,num_jacs)
+    jaccards = np.linspace(0,0.2,num_jacs)
     plot = np.zeros(num_jacs)
     for i in range(num_jacs):
+        print('Starting '+str(i+1)+' out of '+str(num_jacs))
         jac = jaccards[i]
         # do 1 trials for each Jaccard
-        trials = 100
+        trials = 10
         results = np.zeros(trials)
         for j in range(trials):
             # generate 10,000 reads for each
@@ -30,11 +31,15 @@ def main():
             intersection = Similarity.intersection_inclusion_exclusion(h1,h2)
 
             obs_jaccard = intersection/union
-            error = (obs_jaccard - exp_jaccard)
+            error = 100*(obs_jaccard - exp_jaccard)/(1+exp_jaccard)
             results[j] = error
 
         plot[i] = np.mean(results)
 
+    print(plot)
+    plt.title('Jaccard Accuracy for 10,000 reads of length 50')
+    plt.xlabel('Jaccard Index')
+    plt.ylabel('% Error (mean of 10)')
     plt.scatter(jaccards,plot)
     plt.show()
 
