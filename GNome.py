1a3cf57258a64d14b6d2bb014c53023aeee4e1e6
gautam-prab <gautam-prab@users.noreply.github.com>
2019-12-05 01:44:03 -0500

    Add rankings capabilities

--- GNome.py
+++ GNome.py
@@ -4,21 +4,24 @@ import numpy as np
 from HLL import HLL
 import Similarity
 import glob
+import GenomeRankings
 
 
 def get_HLL(reads, numReads):
     hll = HLL(12)
     for i in range(numReads):
-        seq = inputLines[(i*2)+1].rstrip().upper()
+        seq = reads[(i*2)+1].rstrip().upper()
         hll.insert(seq)
     return hll
 
 folder_path = 'Data/0.5x'
 sketches = []
+species = []
 i = 0
 for filename in glob.glob(os.path.join(folder_path, '*.fasta')):
     with open(filename) as myFile:
         inputLines = myFile.readlines()
+    species.append(filename)
     totalReads = int(len(inputLines) / 2)
     sketches.append( get_HLL(inputLines, totalReads))
     i += 1
@@ -32,8 +35,18 @@ for i in range(10):
         h1 = sketches[i]
         h2 = sketches[j]
         union = Similarity.union(h1, h2).cardinality()
-        intersection = Similarity.intersection_inclusion_exclusion(h1, h2)
+        intersection = Similarity.intersection(h1, h2)
         jaccard = intersection / union
         similarity_matrix[i, j] = jaccard
 
+np.set_printoptions(precision = 2, linewidth = 200)
+print('Similarity Matrix:')
 print(similarity_matrix)
+
+rankings = GenomeRankings.rank_genomes(similarity_matrix)
+print('Rankings:')
+for i in range(rankings.shape[0]):
+    print('{}\t\t{}'.format(species[i],str(rankings[i,:])))
+
+# ground_truth = np.array()
+# acc = GenomeRankings.compare_rankings(rankings, ground_truth)
--- GenomeRankings.py
+++ GenomeRankings.py
@@ -1,3 +1,40 @@
 import numpy as np
+from scipy.stats import rankdata
+from scipy.stats import kendalltau
 
-def 
+"""Rank genomes based on similarity
+Input is a square upper triangular matrix of similarity values of a similar form:
+    genome 1    1   s(1,2)  s(1,3)
+    genome 2    0   1       s(2,3)
+    genome 3    0   0       1
+
+Output is a matrix of each genome's rankings:
+    genome 1    rank(1) rank(2) rank(3)
+    genome 2    rank(1) rank(2) rank(3)
+    genome 3    rank(1) rank(2) rank(3)
+"""
+def rank_genomes(sim_matrix):
+    # copy upper triangular matrix into both triangles for ease of use
+    sim_matrix = sim_matrix + sim_matrix.T - np.diagflat(sim_matrix.diagonal())
+
+    # pairwise-rank each genome based on similarities
+    # lower ranks are better matches
+    # each row is a ranking vector for that corresponding genome
+    # ties are ranked with the minimum ranking
+
+    rankings = np.zeros(sim_matrix.shape)
+    # iterate through rows since scipy doesn't have basic matrix functionality
+    for idx,row in enumerate(sim_matrix):
+        rankings[idx,:] = len(row) - rankdata(row, method='min')
+    return rankings
+
+"""Compare two ranking matrices using average Kendall's tau distance
+Outupt values will be between -1 and 1; 1 indicates perfect agreement
+Input: two ranking matrices from the rank_genomes() function"""
+def compare_rankings(mat1, mat2):
+    n = mat1.shape[0]
+    kt_sum = 0
+    for idx in range(n):
+        tau,_ = kendalltau(mat1[idx,:], mat2[idx,:])
+        kt_sum += tau
+    return kt_sum / n
