b15c5aae2b5f16b64461985fbfb27674d830ff12
davidyang2 <davidyang979@gmail.com>
2019-12-06 20:30:30 -0500

    Added command line arguments for easier more customized usage

--- PlotCardinalityAccuracy.py
+++ PlotCardinalityAccuracy.py
@@ -3,36 +3,51 @@ from Random_Generators import Rangen_jaccard
 
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 
-def main():
+"""
+PlotCardinalityAccuracy.main():
+Calculates and plots cardinality percent error over 80 sets of different cardinalities over a logspace range
+Inputs: num_cards - the number of sets
+base_start - Smallest cardinality in range
+base_stop - Largest cardinality in range
+num_trials - number of trials cardinality calculations done for each set
+"""
 
-    num_cards = 80
-    cardinalities = np.logspace(1, 6.7, num_cards)
+def main(num_cards, base_start, base_stop, num_trials, read_length):
+    cardinalities = np.logspace(base_start, base_stop, num_cards)
     plot = np.zeros(num_cards)
     for i in range(num_cards):
         print('Starting '+str(i+1)+' out of '+str(num_cards))
         card = int(cardinalities[i])
         # do 10 trials for each cardinality
-        trials = 10
-        results = np.zeros(trials)
-        for j in range(trials):
+        results = np.zeros(num_trials)
+        for j in range(num_trials):
             h = HLL(12)
             for k in range(card):
-                h.insert(Rangen_jaccard.generate_random_string(40))
+                # Generates a random sequence of length read_length and inserts it into HLL
+                h.insert(Rangen_jaccard.generate_random_string(read_length))
 
-            obs_cardinality = h.cardinality()
+            obs_cardinality = h.cardinality()  # Retrieves the cardinality
 
             results[j] = obs_cardinality
 
-        plot[i] = 100*(np.mean(results)-card)/card
+        plot[i] = 100 * (np.mean(results) - card) / card  # Plots percent error of calculated cardinality vs. expected value
 
     print(plot)
     plt.xscale('log')
     plt.title('Cardinality Accuracy for reads of length 40')
     plt.xlabel('Cardinality')
     plt.ylabel('% Error (mean of 10)')
-    plt.scatter(cardinalities,plot)
+    plt.scatter(cardinalities, plot)
     plt.show()
 
 if (__name__ == '__main__'):
-    main()
+    # Hyperparameters used for this project: num_c = 80, card_s = 1, card_sp = 6.7, num_t = 10, read_length = 40
+    num_c = int(sys.argv[1])
+    card_s = float(sys.argv[2])
+    card_sp = float(sys.argv[3])
+    num_t = int(sys.argv[4])
+    read_length = int(sys.argv[5])
+    print(num_c, card_s, card_sp, num_t, read_length)
+    main(num_c, card_s, card_sp, num_t, read_length)
--- PlotSimilarityAccuracy.py
+++ PlotSimilarityAccuracy.py
@@ -13,6 +13,7 @@ import math
     python PlotSimilarityAccuracy.py intersection to modulate intersection
     python PlotSimilarityAccuracy.py forbes to modulate forbes
     python PlotSimilarityAccuracy.py sd to modulate sorenson-dice
+    Accompanied with appropriate command line argument parameters, described at bottom
 """
 
 """
@@ -26,22 +27,19 @@ num_trials - number of trials (intersection calculations) done for each set's ca
 """
 
 # num_cards, range of cardinalities, number of trials
-def intersection(num_cards, base_start, base_stop, num_trials):
-    # num_cards = 50
-    # 2, 6.7
+def intersection(num_cards, base_start, base_stop, num_trials, exp_jaccard, read_lengths):
     cardinalities = np.logspace(base_start, base_stop, num_cards) # Creates a range of cardinalities for set generation
     plot = np.zeros(num_cards)
     for i in range(num_cards):
         print('Starting ' + str(i + 1) + ' out of ' + str(num_cards))
         card = int(cardinalities[i])
         # do 10 trials for each cardinality
-        # trials = 10
         results = np.zeros(num_trials)
         for j in range(num_trials):
             h1 = HLL(12)
             h2 = HLL(12)
             # Random read (length 40) generator based on the cardinalities and the expected Jaccard value
-            a, b, exp_jaccard, forbes, sd = Rangen_jaccard.generate_reads(0.02, card, card, 40)
+            a, b, exp_jaccard, forbes, sd = Rangen_jaccard.generate_reads(exp_jaccard, card, card, read_lengths)
             for s in a:
                 h1.insert(s)
             for s in b:
@@ -69,21 +67,19 @@ base_start - Smallest cardinality in range
 base_stop - Largest cardinality in range
 num_trials - number of trials (Jaccard calculations) done for each set's cardinality
 """
-def jaccard(num_cards, base_start, base_stop, num_trials):
-    # num_cards = 50
+def jaccard(num_cards, base_start, base_stop, num_trials, exp_jaccard, read_lengths):
     cardinalities = np.logspace(base_start, base_stop, num_cards)  # Generates a range of cardinalities for set generation
     plot = np.zeros(num_cards)
     for i in range(num_cards):
         print('Starting '+str(i+1)+' out of '+str(num_cards))
         card = int(cardinalities[i])
         # do 10 trials for each cardinality
-        # trials = 10
         results = np.zeros(num_trials)
         for j in range(num_trials):
             h1 = HLL(12)
             h2 = HLL(12)
             # Generate reads of length 40 based on the expected Jaccard, 0.02
-            a, b, exp_jaccard, forbes, sd = Rangen_jaccard.generate_reads(0.02, card, card, 40)
+            a, b, exp_jaccard, forbes, sd = Rangen_jaccard.generate_reads(exp_jaccard, card, card, read_lengths)
             for s in a:
                 h1.insert(s)
             for s in b:
@@ -113,7 +109,7 @@ base_start - Smallest cardinality in range
 base_stop - Largest cardinality in range
 num_trials - number of trials (Forbes calculations) done for each set's cardinality
 """
-def forbes(num_cards, base_start, base_stop, num_trials):
+def forbes(num_cards, base_start, base_stop, num_trials, exp_forbes, read_lengths):
     # num_cards = 50
     cardinalities = np.logspace(base_start, base_stop, num_cards) # Generates a range of cardinalities for set generation
     plot = np.zeros(num_cards)
@@ -121,13 +117,12 @@ def forbes(num_cards, base_start, base_stop, num_trials):
         print('Starting '+str(i+1)+' out of '+str(num_cards))
         card = int(cardinalities[i])
         # do 10 trials for each cardinality
-        # trials = 10
         results = np.zeros(num_trials)
         for j in range(num_trials):
             h1 = HLL(12)
             h2 = HLL(12)
             # Generate reads of length 40 based on the expected Forbes, 0.1
-            a,b,exp_forbes = Rangen_forbes.generate_reads(0.1, card, card, 40)
+            a,b,exp_forbes = Rangen_forbes.generate_reads(exp_forbes, card, card, read_lengths)
             for s in a:
                 h1.insert(s)
             for s in b:
@@ -158,21 +153,19 @@ base_start - Smallest cardinality in range
 base_stop - Largest cardinality in range
 num_trials - number of trials (Sorensen-Dice calculations) done for each set's cardinality
 """
-def sd(num_cards, base_start, base_stop, num_trials):
-    # num_cards = 50
+def sd(num_cards, base_start, base_stop, num_trials, exp_sd, read_lengths):
     cardinalities = np.logspace(base_start, base_stop, num_cards) # Generates a range of cardinalities for set generation
     plot = np.zeros(num_cards)
     for i in range(num_cards):
         print('Starting '+str(i+1)+' out of '+str(num_cards))
         card = int(cardinalities[i])
         # do 10 trials for each cardinality
-        # trials = 10
         results = np.zeros(num_trials)
         for j in range(num_trials):
             h1 = HLL(12)
             h2 = HLL(12)
-            # Generate reads of length 40 based on the expected Forbes, 0.04
-            a,b,exp_sd = Rangen_sorenson_dice.generate_reads(0.04, card, card, 40)
+            # Generate reads of length 40 based on the expected Sorensen-Dice, 0.04
+            a,b,exp_sd = Rangen_sorenson_dice.generate_reads(exp_sd, card, card, read_lengths)
             for s in a:
                 h1.insert(s)
             for s in b:
@@ -196,23 +189,30 @@ main()
 Calls the appropriate function based on the command line argument received
 Input: The command-line arguments
 """
-def main(arg, num_c, card_s, card_sp, num_t):
+def main(arg, num_c, card_s, card_sp, num_t, exp_const, read_lengths):
     # Function callers based on command line argument
     if arg == "jaccard":
-        jaccard(num_c, card_s, card_sp, num_t)
+        jaccard(num_c, card_s, card_sp, num_t, exp_const, read_lengths)
     elif arg == "forbes":
-        forbes(num_c, card_s, card_sp, num_t)
+        forbes(num_c, card_s, card_sp, num_t, exp_const, read_lengths)
     elif arg == "sd":
-        sd(num_c, card_s, card_sp, num_t)
+        sd(num_c, card_s, card_sp, num_t, exp_const, read_lengths)
     elif arg == "intersection":
-        intersection(num_c, card_s, card_sp, num_t)
+        intersection(num_c, card_s, card_sp, num_t, exp_const, read_lengths)
 
 
 if (__name__ == '__main__'):
-    # Hyperparameters we used for this project: num_c = 50, card_s = 2, card_sp = 6.7, 
+    # Hyperparameters we used for this project: num_c = 50, card_s = 2, card_sp = 6.7, num_t = 10
+    # Specific hyperparameters we used for the different methods:
+    # Intersection and Jaccard: exp_const = 0.02, read_lengths = 40
+    # Forbes: exp_const = 0.1, read_lengths = 40
+    # Sorensen-Dice: exp_const = 0.04, read_lengths = 40
     arg = sys.argv[1]
     num_c = int(sys.argv[2])
     card_s = float(sys.argv[3])
     card_sp = float(sys.argv[4])
     num_t = int(sys.argv[5])
-    main(arg, num_c, card_s, card_sp, num_t)
+    exp_const = float(sys.argv[6])
+    read_lengths = int(sys.argv[7])
+    print(arg, num_c, card_s, num_t, exp_const, read_lengths)
+    main(arg, num_c, card_s, card_sp, num_t, exp_const, read_lengths)
