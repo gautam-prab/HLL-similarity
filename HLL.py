1a5b0e0b0a05ba4a5f3e51f5763a50e0d9c718e3
davidyang2 <davidyang979@gmail.com>
2019-12-06 19:07:10 -0500

    Added comments

--- HLL.py
+++ HLL.py
@@ -10,9 +10,9 @@ class HLL:
     """HyperLogLog implementation"""
 
     def __init__(self, p, registers=None):
-        """Initialize HLL with 2^p registers"""
+        # Initialize HLL with 2^p registers
         self.p = p
-        self.q = 64 - p # assuming 64-bit hash value
+        self.q = 64 - p  # assuming 64-bit hash value
         self.m = 2 ** p
 
         if registers != None:
@@ -21,11 +21,18 @@ class HLL:
         else:
             self.registers = [0 for i in range(self.m)]
 
+    """
+    HLL.insert():
+    Inserts reads into the HLL, utilizes a uniform hashing function and methods
+    Comes from Algorithm 1 in Ertl paper
+    
+    Input: The HLL, the read
+    """
     def insert(self, read):
-        """Insert read into the HLL"""
-        # read is the sequence, convert read to base 4 -> base 10 -> base 2
-        read.upper()
+        # Insert read into the HLL, convert read to base 4 -> base 10 -> base 2
+        read.upper()  # Capitalization ensures proper conversion
         b_four_read = ""
+        # Base 4 conversion
         for b in read:
             if b == 'A':
                 b_four_read += "0"
@@ -39,16 +46,19 @@ class HLL:
         b_ten_read = 0
         read_len = len(b_four_read)
 
+        # Base 10 conversion
         for c in b_four_read:
             b_ten_read = b_ten_read + (int(c) * (4 ** (read_len - 1)))
             read_len = read_len - 1
 
+        # Call to hash function, returns a 64-bit hash value
         read_hash = Hash.hash64shift(b_ten_read)
 
         read_hash = "{0:b}".format(read_hash)  # Converts into binary
 
         len_read_hash = len(read_hash)
 
+        # Ensures proper length of the hashed value in case the value is too small for extend to 64 bits
         if len_read_hash < 64:
             zeroes = "".join(["0"] * (64 - len_read_hash))
             read_hash = zeroes + read_hash
@@ -56,23 +66,46 @@ class HLL:
         a = read_hash[0:self.p]  # First p bits of (p + q)-bit hash value of read
         b = read_hash[self.p:]  # Following q bits of (p + q)-bit hash value of read
 
-        k = b.find('1')+1 # be sure to add 1 to match match from Ertl paper
+        k = b.find('1') + 1  # be sure to add 1 to match match from Ertl paper
         if k == -1:
             k = self.q + 1
 
-        i = int(a, 2)
+        i = int(a, 2)  # Indexing into HLL
 
+        # Insert into HLL
         if k > self.registers[i]:
             self.registers[i] = k
         return
 
+    """
+    HLL.cardinality():
+    Calls the getMultiplicity() function to get the count vector from HLL registers
+    Calls the estimateCardinality() function to calculate the maximum likelihood estimator for the cardinality of a HLL
+    
+    Input: The HLL
+    Output: The cardinality estimate
+    """
     def cardinality(self):
         C = Cardinality.getMultiplicity(self)
         return Cardinality.estimateCardinality(C)
 
+    """
+    HLL.simple_cardinality():
+    Calls the simpleCardinality() function to calculate cardinality of HLL by bias-corrected harmonic mean
+    
+    Input: The HLL
+    Output: The cardinality estimate
+    """
     def simple_cardinality(self):
         return Cardinality.simpleCardinality(self.getRegisters())
 
+    """
+    HLL.getRegisters():
+    Returns the registers of the HLL, a list of values
+    
+    Input: The HLL
+    Output: The registers
+    """
     def getRegisters(self):
         return self.registers
 
--- Hash.py
+++ Hash.py
@@ -1,9 +1,30 @@
+
+"""
+Hash.rshift32()
+Account for integer overflow, replaces the ">>>" operator found in
+Thomas Wang's implementation since Python does not do overflow for integers
+Input: The value to be hashed, the number to shift right by
+Output: The shifted value
+"""
 def rshift32(val, n): return (val % 0x100000000) >> n
 
+"""
+Hash.rshift64()
+Account for integer overflow, replaces the ">>>" operator found in 
+Thomas Wang's implementation since Python does not do overflow for integers
+Input: The value to be hashed, the number to shift right by
+Output: The shifted value
+"""
 def rshift64(val, n): return (val % 0x10000000000000000) >> n
 
 # Source: Thomas Wang, March 2007
 # https://gist.github.com/badboy/6267743
+
+"""
+Hash.hash64shift()
+Creates a 64-bit hash value, uniform hashing function
+Input: The value to be hashed
+"""
 def hash64shift(key):
     key = (~key) + (key << 21)  # key = (key << 21) - key - 1
     key = key ^ (rshift64(key, 24))
@@ -13,6 +34,12 @@ def hash64shift(key):
     key = key ^ (rshift64(key, 28))
     key = (key + (key << 31)) % 0x10000000000000000
     return key
+
+"""
+Hash.hash32shift()
+Creates a 32-bit hash value, uniform hashing function
+Input: The value to be hashed
+"""
 
 def hash32shift(key):
     key = ~key + (key << 15)  # key = (key << 15) - key - 1;
